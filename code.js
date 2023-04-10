d043d3320c3990e2599c1766e6dc5c62fc265237
James Bradford <j.bradford@pnnl.gov>
2023-04-10 13:22:56 -0700

    validation happens during input for better UX

--- code.js
+++ code.js
@@ -1,12 +1,26 @@
 figma.parameters.on("input", onInput);
 figma.on("run", onRun);
 
-function onInput({ result }) {
-  // console.log({ parameters, key, query, result });
+function onInput({ parameters, key, query, result }) {
+  const percentage = parseInt(query) / 100;
 
-  // TODO: validate number here instead, provide as single suggestion
-  
-  result.setSuggestions([]); // freeform
+  // console.log({ parameters, key, query, result, percentage });
+
+  if (figma.currentPage.selection.length === 0) {
+    result.setLoadingMessage("No Layers Selected...");
+  } else if (!query) {
+    result.setSuggestions([]);
+  } else if (typeof percentage !== "number" || isNaN(percentage)) {
+    result.setLoadingMessage("Input must be a number");
+  } else if (percentage <= 0) {
+    result.setLoadingMessage("Input must be more than 0%");
+  } else if (percentage >= 1) {
+    result.setLoadingMessage("Input must be less than 100%");
+  } else {
+    const zero = percentage < 0.1 ? "0" : "";
+    const percent100 = percentage * 100;
+    result.setSuggestions([`${zero}${percent100}%`]);
+  }
 }
 
 function onRun({ parameters }) {
@@ -15,34 +29,24 @@ function onRun({ parameters }) {
   // validate number
   const percentage = parseInt(parameters.percentage) / 100;
 
-  if (figma.currentPage.selection.length === 0) {
-    figma.notify("Nothing Selected");
-  } else if (typeof percentage !== "number" || isNaN(percentage)) {
-    figma.notify("Input not a number. Selection unchanged.");
-  } else if (percentage <= 0) {
-    figma.currentPage.selection = [];
-    figma.notify("Input was less than 0%. Selected nothing.");
-  } else if (percentage >= 1) {
-    figma.notify("Input was greater than 100%. Selection unchanged.");
-  } else {
-    if (
-      figma.currentPage.selection.length === 1 &&
-      figma.currentPage.selection[0].children != null
-    ) {
-      // if the selection is a single element with children, sub-select the children instead
-      figma.currentPage.selection = figma.currentPage.selection[0].children;
-    }
-
-    const originalLength = figma.currentPage.selection.length;
-    figma.currentPage.selection = figma.currentPage.selection.filter(
-      (node) => Math.random() < percentage
-    );
-    const newLength = figma.currentPage.selection.length;
-    const percent100 = percentage * 100;
-    figma.notify(
-      `Selected ${newLength} of ${originalLength} layers. About ${percent100}%`
-    );
+  if (
+    figma.currentPage.selection.length === 1 &&
+    figma.currentPage.selection[0].children != null
+  ) {
+    // if the selection is a single element with children, sub-select the children instead
+    figma.currentPage.selection = figma.currentPage.selection[0].children;
   }
+
+  const originalLength = figma.currentPage.selection.length;
+  figma.currentPage.selection = figma.currentPage.selection.filter(
+    (node) => Math.random() < percentage
+  );
+  const newLength = figma.currentPage.selection.length;
+  const zero = percentage < 0.1 ? "0" : "";
+  const percent100 = percentage * 100;
+  figma.notify(
+    `Selected ${newLength} of ${originalLength} layers. About ${zero}${percent100}%`
+  );
 
   figma.closePlugin();
 }
